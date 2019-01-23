6c8112cd07283e1aae41876da10513358bcc6ae2
timothyhalley <timothy.halley@gmail.com>
2019-01-23 14:49:19 -0800

    V5.2 DBDynamo troubles :)

--- auto/appawsdb.js
+++ auto/appawsdb.js
@@ -1,5 +1,6 @@
 var AWS = require("aws-sdk");
 
+
 AWS.config.update({
   region: "us-west-2"
 });
@@ -9,30 +10,12 @@ var dynamodb = new AWS.DynamoDB();
 var params = {
     TableName : "Photos",
     KeySchema: [
-        { AttributeName: "key", KeyType: "HASH"},  //Partition key
-        { AttributeName: "DTepoch", KeyType: "RANGE" }  //Sort key
+        { AttributeName: "album", KeyType: "HASH" },
+        { AttributeName: "key", KeyType: "RANGE"}
     ],
     AttributeDefinitions: [
         { AttributeName: "album", AttributeType: "S" },
-        { AttributeName: "key", AttributeType: "S" },
-        { AttributeName: "name", AttributeType: "S" },
-        { AttributeName: "ext", AttributeType: "S" },
-        { AttributeName: "type", AttributeType: "S" },
-        { AttributeName: "mime", AttributeType: "S" },
-        { AttributeName: "dir", AttributeType: "S" },
-        { AttributeName: "directory", AttributeType: "S" },
-        { AttributeName: "DTepoch", AttributeType: "N" },
-        { AttributeName: "DTcirca", AttributeType: "S" },
-        { AttributeName: "address0", AttributeType: "S" },
-        { AttributeName: "address1", AttributeType: "S" },
-        { AttributeName: "timeZone", AttributeType: "S" },
-        { AttributeName: "GPSPosition", AttributeType: "S" },
-        { AttributeName: "GPSLatitude", AttributeType: "S" },
-        { AttributeName: "GPSLongitude", AttributeType: "S" },
-        { AttributeName: "origSize", AttributeType: "S" },
-        { AttributeName: "origWidth", AttributeType: "S" },
-        { AttributeName: "origHeight", AttributeType: "S" },
-        { AttributeName: "origBtyes", AttributeType: "S" }
+        { AttributeName: "key", AttributeType: "S" }
     ],
     ProvisionedThroughput: {
         ReadCapacityUnits: 10,
